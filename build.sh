41890c8747329709e3f9fc58de24ec4c1c47313a
Hideaki Ibuki <ibuki90@gmail.com>
2019-05-02 05:18:36 +0000

    test comit

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-echo "Hello world"
+DOCKERILES=`find ./ -name Dockerfile`
+for DOCKERILE in $DOCKERILES
+do
+    DOCKERDIR=`dirname $DOCKERILE`
+    git diff --stat HEAD^ $DOCKERDIR
+    if [ $? = 1 ]; then
+        echo 'aa'
+    fi
+done
